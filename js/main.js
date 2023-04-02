9fdcbcb681c89ffee9f569b464714a10db3f242e
Jose A. Carrillo <josecarrillo1991.jc@gmail.com>
2023-04-02 18:50:38 -0500

    feat: add mobille menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 
-menuEmail.addEventListener('click', toggleMenu)
+const IconMenu = document.querySelector('.icon-menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
+menuEmail.addEventListener('click', toggleMenu)
+IconMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
