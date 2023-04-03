17c23542fb75dd62985958f35d10cde5523cdcdc
Jose A. Carrillo <josecarrillo1991.jc@gmail.com>
2023-04-02 21:06:37 -0500

    feat: add product detail

--- js/main.js
+++ js/main.js
@@ -4,13 +4,43 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const IconMenu = document.querySelector('.icon-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 
+const shoppingCart = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail')
+
 menuEmail.addEventListener('click', toggleMenu)
 IconMenu.addEventListener('click', toggleMobileMenu)
+shoppingCart.addEventListener('click', shoppingCartAside)
 
 function toggleMenu() {
-    desktopMenu.classList.toggle('inactive')
+    const isProductDetailClosed = productDetail.classList.contains('inactive')
+    
+    if (!isProductDetailClosed) {
+        productDetail.classList.toggle('inactive')
+    }
+
+    return desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive')
+    const isProductDetailClosed = productDetail.classList.contains('inactive')
+    
+    if (!isProductDetailClosed) {
+        productDetail.classList.toggle('inactive')
+    }
+
+    return mobileMenu.classList.toggle('inactive')
+}
+
+function shoppingCartAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.toggle('inactive')
+    }
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.toggle('inactive')
+    }
+
+    return productDetail.classList.toggle('inactive')
 }
