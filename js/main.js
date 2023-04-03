a09a7b39458d09f72201f31a929dd497abeca477
Jose A. Carrillo <josecarrillo1991.jc@gmail.com>
2023-04-02 21:55:26 -0500

    feat: add product list

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ const mobileMenu = document.querySelector('.mobile-menu')
 const shoppingCart = document.querySelector('.navbar-shopping-cart')
 const productDetail = document.querySelector('.product-detail')
 
+const cardsContainer = document.querySelector('.cards-container')
+
 menuEmail.addEventListener('click', toggleMenu)
 IconMenu.addEventListener('click', toggleMobileMenu)
 shoppingCart.addEventListener('click', shoppingCartAside)
@@ -44,3 +46,109 @@ function shoppingCartAside() {
 
     return productDetail.classList.toggle('inactive')
 }
+
+const productItems = [
+    {
+        name: 'Bike',
+        price: 50000,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        name: 'Peluche',
+        price: 75000,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        name: 'Balon',
+        price: 10000,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        name: 'Parques',
+        price: 15000,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        name: 'Bike',
+        price: 50000,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        name: 'Peluche',
+        price: 75000,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        name: 'Balon',
+        price: 10000,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        name: 'Parques',
+        price: 15000,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },     {
+        name: 'Bike',
+        price: 50000,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        name: 'Peluche',
+        price: 75000,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        name: 'Balon',
+        price: 10000,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        name: 'Parques',
+        price: 15000,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }
+]
+productItems.push({
+    name: 'Triciclo',
+    price: 20000,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+function renderProductos(array) {
+    array.forEach((element) => {
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+    
+        const imgProduct = document.createElement('img')
+        imgProduct.setAttribute('src', element.image)
+    
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+    
+        const div = document.createElement('div')
+    
+        const productPrice = document.createElement('p')
+        productPrice.innerText = '$' + element.price
+        const productName = document.createElement('p')
+        productName.innerHTML = element.name
+    
+        const productFigure = document.createElement('figure')
+        const shoppingCart = document.createElement('img')
+        shoppingCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+    
+    
+    
+        productInfo.appendChild(div)
+        productInfo.appendChild(productFigure)
+        div.appendChild(productPrice)
+        div.appendChild(productName)
+        productFigure.appendChild(shoppingCart)
+    
+        productCard.appendChild(imgProduct)
+        productCard.appendChild(productInfo)
+    
+        cardsContainer.appendChild(productCard)
+    });
+}
+
+renderProductos(productItems)
